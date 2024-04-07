80ae6225ff0b456e65289bb4a6c081e4460d229f
Edu0710 <165351861+Edu0710@users.noreply.github.com>
2024-04-07 20:44:33 +0200

    Update front.py

--- front.py
+++ front.py
@@ -172,13 +172,13 @@ def sankey_plot(historico_padreadas):
     # Asignar un color único a cada nodo
     color_palette = [
     'rgba(72, 61, 139, 0.7)',  # Azul índigo translúcido
-    'rgba(160, 82, 45, 0.6)',  # Marrón rojizo translúcido
+    'rgba(128, 0, 0, 0.6)',  # Marrón rojizo translúcido
     'rgba(128, 128, 128, 0.6)',# Gris oscuro translúcido
     'rgba(47, 79, 79, 0.6)',   # Verde pizarra translúcido
     'rgba(139, 0, 0, 0.6)',    # Rojo oscuro translúcido
     'rgba(25, 25, 112, 0.6)',  # Azul medianoche translúcido
     'rgba(139, 0, 0, 0.6)',  # Marrón oscuro translúcido
-     'rgba(0, 0, 139, 0.5)',    # Azul oscuro translúcido
+    'rgba(72, 61, 139, 0.7)',    # Azul oscuro translúcido
     'rgba(139, 69, 19, 0.6)',  # Marrón oscuro translúcido
     'rgba(0, 100, 0, 0.5)',    # Verde oscuro translúcido
     'rgba(139, 0, 139, 0.7)',  # Púrpura oscuro translúcido
