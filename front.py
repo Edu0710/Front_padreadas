e8ad307ec4e0981e984c958d3572f74ebc037a1b
Edu0710 <165351861+Edu0710@users.noreply.github.com>
2024-04-07 20:28:29 +0200

    Update front.py

--- front.py
+++ front.py
@@ -157,8 +157,7 @@ def sankey_plot(historico_padreadas):
     df = historico_padreadas[['Faltoso','Víctima','Puntos']]
     df = df.groupby(['Faltoso','Víctima']).sum()
     df.reset_index ( drop = False, inplace = True)
-    st.dataframe(df)
-    df['Víctima'] = df['Víctima'] + '_2'
+    df['Víctima'] = df['Víctima'] + '_'
     nodes = list(set(df['Faltoso']).union(set(df['Víctima'])))
 
     # Crear un diccionario para asignar índices a cada nodo único
@@ -173,20 +172,20 @@ def sankey_plot(historico_padreadas):
     # Asignar un color único a cada nodo
     color_palette = [
     'rgba(0, 0, 139, 0.5)',    # Azul oscuro translúcido
-    'rgba(139, 69, 19, 0.5)',  # Marrón oscuro translúcido
+    'rgba(139, 69, 19, 0.6)',  # Marrón oscuro translúcido
     'rgba(0, 100, 0, 0.5)',    # Verde oscuro translúcido
-    'rgba(139, 0, 139, 0.5)',  # Púrpura oscuro translúcido
-    'rgba(128, 0, 0, 0.5)',    # Rojo oscuro translúcido
-    'rgba(128, 128, 0, 0.5)',  # Verde oliva translúcido
-    'rgba(139, 0, 0, 0.5)',    # Rojo oscuro translúcido
-    'rgba(46, 139, 87, 0.5)',  # Verde mar translúcido
-    'rgba(72, 61, 139, 0.5)',  # Azul índigo translúcido
-    'rgba(160, 82, 45, 0.5)',  # Marrón rojizo translúcido
-    'rgba(128, 128, 128, 0.5)',# Gris oscuro translúcido
-    'rgba(47, 79, 79, 0.5)',   # Verde pizarra translúcido
-    'rgba(139, 0, 0, 0.5)',    # Rojo oscuro translúcido
-    'rgba(25, 25, 112, 0.5)',  # Azul medianoche translúcido
-    'rgba(139, 69, 19, 0.5)'   # Marrón oscuro translúcido
+    'rgba(139, 0, 139, 0.7)',  # Púrpura oscuro translúcido
+    'rgba(128, 0, 0, 0.6)',    # Rojo oscuro translúcido
+    'rgba(128, 128, 0, 0.7)',  # Verde oliva translúcido
+    'rgba(139, 0, 0, 0.6)',    # Rojo oscuro translúcido
+    'rgba(46, 139, 87, 0.7)',  # Verde mar translúcido
+    'rgba(72, 61, 139, 0.7)',  # Azul índigo translúcido
+    'rgba(160, 82, 45, 0.6)',  # Marrón rojizo translúcido
+    'rgba(128, 128, 128, 0.6)',# Gris oscuro translúcido
+    'rgba(47, 79, 79, 0.6)',   # Verde pizarra translúcido
+    'rgba(139, 0, 0, 0.6)',    # Rojo oscuro translúcido
+    'rgba(25, 25, 112, 0.6)',  # Azul medianoche translúcido
+    'rgba(139, 0, 0, 0.6)'   # Marrón oscuro translúcido
 ]
     # Iterar sobre cada nodo para asignar colores
     for node in nodes:
