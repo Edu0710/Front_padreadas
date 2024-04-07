d6051916775006037b829121fd5741d39f7e373c
Edu0710 <165351861+Edu0710@users.noreply.github.com>
2024-04-07 20:15:44 +0200

    Update front.py

--- front.py
+++ front.py
@@ -202,7 +202,7 @@ def sankey_plot(historico_padreadas):
     ))
 
     # Actualizar diseño y título del gráfico
-    fig.update_layout(title_text="Gráfico Sankey de Relaciones",
+    fig.update_layout(title_text="Gráfico Sankey",
                       font=dict(size=12, color="black"))
 
     # Mostrar el gráfico utilizando Streamlit
