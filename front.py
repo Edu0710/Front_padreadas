6fef5c1a06261913c17ea08ab3075a3b8576db69
Edu0710 <165351861+Edu0710@users.noreply.github.com>
2024-04-07 21:03:44 +0200

    Update front.py

--- front.py
+++ front.py
@@ -182,8 +182,8 @@ def sankey_plot(historico_padreadas):
     'rgba(139, 69, 19, 0.6)',   # Marrón oscuro translúcido
     'rgba(128, 128, 0, 0.7)',   # Verde oliva translúcido
     'rgba(139, 0, 0, 0.6)',     # Otro rojo oscuro translúcido
-    'rgba(128, 0, 0, 0.6)',     # Otro rojo oscuro translúcido
-    'rgba(128, 0, 0, 0.6)',     # Otro rojo oscuro translúcido
+    'rgba(128, 30, 20, 0.6)',     # Otro rojo oscuro translúcido
+    'rgba(128, 30, 20, 0.6)',     # Otro rojo oscuro translúcido
     'rgba(139, 0, 0, 0.6)',     # Otro rojo oscuro translúcido
 ]
     # Iterar sobre cada nodo para asignar colores
